84c7a22fc02ce6226064b1765e742d91bd435ca2
IEstudiante <ibayaa@estudiante.edib.es>
2024-11-14 13:27:19 +0100

    Mi rama (#38)
    * Prueba formatos
    
    * Mas pruebas de formateo
    
    * Aun más pruebas de formateo
    
    * quitar console log
    
    * log in sofisticado
    
    * leer empleados.json

--- server_js/server.js
+++ server_js/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -12,13 +14,60 @@ app.get('/usuarios', (req, res) => {
   const filePath = path.join(__dirname, 'usuarios.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      res.status(500).send('Error al leer el archivo');
+      console.error('Error reading file:', err);
+      res.status(500).send('Error reading the file');
     } else {
       res.send(data);
     }
   });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+app.get('/empleados', (req, res) => {
+  const filePath = path.join(__dirname, 'empleados.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      res.status(500).send('Error reading the file');
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
+
+app.delete('/empleados/:id', (req, res) => {
+  const filePath = path.join(__dirname, 'empleados.json');
+  const employeeId = req.params.id;
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).send('Error reading the file');
+    }
+
+    let employees = JSON.parse(data).empleados;
+    const filteredEmployees = employees.filter(employee => employee.id !== employeeId);
+
+    fs.writeFile(filePath, JSON.stringify({ empleados: filteredEmployees }, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+        return res.status(500).send('Error writing the file');
+      }
+
+      res.status(200).send('Employee deleted successfully');
+    });
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(projectDir, 'admin.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
