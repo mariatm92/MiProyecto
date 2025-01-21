23fc2c41d81ef81397e4d34de12f6d410991f250
maria <m.tomas1992@hotmail.com>
2025-01-21 16:21:50 +0100

    actualizado productos

--- server_js/server.js
+++ server_js/server.js
@@ -17,6 +17,21 @@ const projectRoot = path.join(__dirname, '..');
 app.use(express.static(projectRoot));
 
 
+
+app.get('/habitaciones', (req, res) => {
+  const filePath = path.join(__dirname, 'habitaciones.json');
+  console.log('Intentando leer el archivo:', filePath); // Mensaje de depuración
+  fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+          console.error('Error leyendo el archivo:', err);
+          res.status(500).send('Error reading the file');
+      } else {
+          res.json(JSON.parse(data));
+      }
+  });
+});
+
+
 app.get('/servicios', (req, res) => {
   const filePath = path.join(__dirname, 'servicios.json');
   console.log('Intentando leer el archivo:', filePath); // Mensaje de depuración
