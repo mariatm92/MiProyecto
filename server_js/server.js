4c2081bdeb983ab73562b313946af6a72f5a8962
maria <m.tomas1992@hotmail.com>
2025-01-24 09:20:42 +0100

    jjfjf

--- server_js/server.js
+++ server_js/server.js
@@ -264,12 +264,10 @@ app.delete('/habitaciones/delete/:id', (req, res) => {
             }
             res.status(200).send('Habitación eliminada');
         });
-    });
-    
+      });
 });
 
 
-////// ESTE BOTON NO FUNCIONA ///////////////////////////////////////////////////////////////////////
 
 // Endpoint to delete a product from servicios.json
 app.delete('/servicios/delete/:id', (req, res) => {
@@ -334,11 +332,11 @@ app.post('/stock/:tipo', (req, res) => {
     });
 });
 
-//ENDPOINTS PARA AGREGAR
-// Endpoint to add a product to habitaciones.json
-app.post('/habitaciones', (req, res) => {
+// Endpoint para modificar habitaciones
+app.put('/habitaciones/modify/:id', (req, res) => {
     const filePath = path.join(__dirname, 'habitaciones.json');
-    const nuevoProducto = req.body;
+    const id = req.params.id;
+    const updatedData = req.body;
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
@@ -346,21 +344,32 @@ app.post('/habitaciones', (req, res) => {
         }
 
         let habitacionesData = JSON.parse(data);
-        habitacionesData.habitaciones.push(nuevoProducto);
+        const habitacionIndex = habitacionesData.habitaciones.findIndex(h => h.id === Number(id));
+
+        if (habitacionIndex === -1) {
+            return res.status(404).send('Habitación no encontrada');
+        }
+
+        // Actualizar la habitación
+        habitacionesData.habitaciones[habitacionIndex] = {
+            ...habitacionesData.habitaciones[habitacionIndex],
+            ...updatedData
+        };
 
         fs.writeFile(filePath, JSON.stringify(habitacionesData, null, 2), (err) => {
             if (err) {
                 return res.status(500).send('Error al escribir el archivo de habitaciones');
             }
-            res.status(201).json(nuevoProducto);
+            res.status(200).json(habitacionesData.habitaciones[habitacionIndex]);
         });
     });
 });
 
-// Endpoint to add a product to servicios.json
-app.post('/servicios', (req, res) => {
+// Endpoint para modificar servicios
+app.put('/servicios/modify/:id', (req, res) => {
     const filePath = path.join(__dirname, 'servicios.json');
-    const nuevoProducto = req.body;
+    const id = req.params.id;
+    const updatedData = req.body;
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
@@ -368,18 +377,26 @@ app.post('/servicios', (req, res) => {
         }
 
         let serviciosData = JSON.parse(data);
-        serviciosData.push(nuevoProducto);
+        const servicioIndex = serviciosData.findIndex(s => s.id === Number(id));
+
+        if (servicioIndex === -1) {
+            return res.status(404).send('Servicio no encontrado');
+        }
+
+        // Actualizar el servicio
+        serviciosData[servicioIndex] = {
+            ...serviciosData[servicioIndex],
+            ...updatedData
+        };
 
         fs.writeFile(filePath, JSON.stringify(serviciosData, null, 2), (err) => {
             if (err) {
                 return res.status(500).send('Error al escribir el archivo de servicios');
             }
-            res.status(201).json(nuevoProducto);
+            res.status(200).json(serviciosData[servicioIndex]);
         });
     });
 });
-
-//FIN ENDPOINTS PARA AGREGAR
 
 
 // Configuración del puerto del servidor
